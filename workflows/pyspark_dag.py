e5eb2c6adb2333d6e7206d4572768f0e0d6dc6cd
DevProj11 <nsindhuja530@gmail.com>
2026-04-12 13:36:41 -0400

    bucket name changed

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "project-dd3b2aab-6af2-4164-8a1"
 REGION = "us-east1"
 CLUSTER_NAME = "dpcluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-85a99495-bucket" ## change this 
+COMPOSER_BUCKET = "us-central1-demo-instance-118d71c9-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
